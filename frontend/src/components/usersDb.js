47dba1dabc13ca81e8900bb9f3e2f44ba222a90c
AnB School 0015 <anbschool0015@AnBs-Mac-mini-26.local>
2024-09-09 11:19:56 +0700

    me

--- frontend/src/components/usersDb.js
+++ frontend/src/components/usersDb.js
@@ -6,33 +6,7 @@ export const Users = [
         userName : "Johny69",
         email : "john.doe@example.com",
         password : "password123",
-        name : firstName + lastName,
         gender : "lesbian",
         role : "admin"
     },
-    {
-        id : 1234234,
-        firstName : "Jack",
-        lastName  : "Doe",
-        userName : "Johny69",
-        email : "Jack.doe@example.com",
-        password : "password123",
-        name : firstName + lastName,
-        gender : "male",
-        role : "developer",
-        entity : "University",
-        localtion : "Phnom Penh"
-    },
-    {
-        id : 1234234,
-        firstName : "Jeep",
-        lastName  : "Doe",
-        userName : "Jeep69",
-        email : "Jeep.doe@example.com",
-        password : "password123",
-        name : firstName + lastName,
-        gender : "female",
-        role : "user"
-    }
-
 ]
